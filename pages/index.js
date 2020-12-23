528d11ecb1b5463af3fec219c43a8f19221274e5
Emanual Jade <emanualjade@gmail.com>
2020-12-23 10:09:48 -0800

    Uppercase component names

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "../components/Date";
 
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
