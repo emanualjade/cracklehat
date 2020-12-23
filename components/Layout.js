c0ada4a50f81ffef7573ac42fabe70694ed5fc25
Emanual Jade <emanualjade@gmail.com>
2020-12-23 14:31:03 -0800

    Little title change

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "CrackleHat";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "A place for random things";
 
 export default function Layout({ children, home }) {
   return (
